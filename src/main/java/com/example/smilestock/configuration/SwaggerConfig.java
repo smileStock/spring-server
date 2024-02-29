9aa636241e58fa113fceb0b2293648ba025611ab
JunhyeonKwak <jhkwak905@naver.com>
2024-02-29 16:07:17 +0900

    feat: swagger 추가

---
+++ src/main/java/com/example/smilestock/configuration/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.example.smilestock.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "Couple App",
+                description = "couple app api명세",
+                version = "v1"))
+@Configuration
+public class SwaggerConfig {
+    // http://localhost:8080/swagger-ui/index.html
+    @Bean
+    public GroupedOpenApi chatOpenApi() {
+        String[] paths = {"/v1/**"};
+
+        return GroupedOpenApi.builder()
+                .group("COUPLE API v1")
+                .pathsToMatch(paths)
+                .build();
+    }
+}
