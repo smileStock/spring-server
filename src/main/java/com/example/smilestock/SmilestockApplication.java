0a18a9c68cf99d6d3b08e489e6bbfb1aec229ebf
JunhyeonKwak <jhkwak905@naver.com>
2024-02-29 15:37:40 +0900

    first commit

---
+++ src/main/java/com/example/smilestock/SmilestockApplication.java
@@ -0,0 +1,13 @@
+package com.example.smilestock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmilestockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmilestockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/smilestock/SmilestockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smilestock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmilestockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
