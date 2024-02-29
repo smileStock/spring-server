427ffbd7dc5701e727942f4241e8cba208dd62ec
Anyohan <dygks3240@hanyang.ac.kr>
2024-02-29 17:36:55 +0900

    fix swagger 수정
    swagger 수정

--- src/main/java/com/example/smilestock/controller/HelloController.java
+++ src/main/java/com/example/smilestock/controller/HelloController.java
@@ -3,13 +3,19 @@ package com.example.smilestock.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+
+
+@RestController
+
 public class HelloController {
 
-    @GetMapping("")
+    @GetMapping("/")
+
     public String hello(){
-        return "index";
+        return "hello";
     }
 }
