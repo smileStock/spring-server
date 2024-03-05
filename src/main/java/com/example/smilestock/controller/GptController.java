edaaaede2f6617eab8bd8eccb30186b9c2f0721a
JunhyeonKwak <jhkwak905@naver.com>
2024-03-05 15:58:34 +0900

    feat: gpt 의존성 주입

--- src/main/java/com/example/smilestock/controller/GptController.java
+++ src/main/java/com/example/smilestock/controller/GptController.java
@@ -2,19 +2,23 @@ package com.example.smilestock.controller;
 
 
 import com.example.smilestock.service.ChatService;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequiredArgsConstructor
 @RestController
 @Slf4j
 @RequestMapping("/api/v1/chat-gpt")
 public class GptController {
     private final ChatService chatService;
+
+    @Autowired
+    public GptController(ChatService chatService) {
+        this.chatService = chatService;
+    }
 
     //chat-gpt 와 간단한 채팅 서비스 소스
     @PostMapping("")
--- src/main/java/com/example/smilestock/service/ChatService.java
+++ src/main/java/com/example/smilestock/service/ChatService.java
@@ -1,13 +1,17 @@
 package com.example.smilestock.service;
 
 import io.github.flashvayne.chatgpt.service.ChatgptService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class ChatService {
     private final ChatgptService chatgptService;
+
+    @Autowired
+    public ChatService(ChatgptService chatgptService) {
+        this.chatgptService = chatgptService;
+    }
 
     public String getChatResponse(String prompt) {
         // ChatGPT 에게 질문을 던집니다.
