96d3ab4441bbefce527f77aa4e534d640d428ff1
JunhyeonKwak <jhkwak905@naver.com>
2024-03-07 16:46:49 +0900

    feat: 재무 관련 DB 생성 및 관계 추가

--- src/main/java/com/example/smilestock/entity/AnalysisEntity.java
+++ src/main/java/com/example/smilestock/entity/AnalysisEntity.java
@@ -7,15 +7,13 @@ import lombok.Setter;
 @Entity
 @Setter
 @Getter
-public class StockEntity {
+public class AnalysisEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    private String corpCode;
-
-    private String stockCode;
-
-    @OneToOne
-    private AnalysisEntity analysisEntity;
+    private String Year;
+    private String reportCode;
+    private String analysisResult;
+    @OneToOne(mappedBy = "analysisEntity")
+    private StockEntity stockEntity;
 }
